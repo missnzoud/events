61a93642941bdb8eef594dc4b68acbc42540b69f
michelle <nzoudmich@yahoo.fr>
2018-08-29 16:06:13 -0700

    you can see the differente categories as a list

--- js/ui.js
+++ js/ui.js
@@ -12,7 +12,18 @@ class UI {
         printCategories() {
             const categoriesList = eventbrite.getCategoriesAPI()
             .then(categories => {
-                 console.log(categories);
+                 //console.log(categories.categories.categories);
+                 const categoriesList = categories.categories.categories;
+                 const categoriesSelect = document.querySelector('#category');    
+                 // Inserts categories into select
+                 categoriesList.forEach(category => {
+                      // Create the options
+                      const option = document.createElement('option');
+                      option.value = category.id;
+                      option.appendChild(document.createTextNode(category.name));
+                      categoriesSelect.appendChild(option);
+                 })
+
             })
         }
 
