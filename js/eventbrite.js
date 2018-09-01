d37aa036686ed1d7a8e155389a8ca6d0506b1d4b
michelle <nzoudmich@yahoo.fr>
2018-09-01 04:28:00 -0700

    end of the projet

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -2,12 +2,28 @@ class EventBrite {
  // Constructor when instanciate
  constructor() {
     this.auth_token = '3525E7NSSPB5WOOTO42V';
+    this.orderby = 'date';
     
 }
+
+ // Get the Events from API
+
+ async queryAPI(eventName, category) {
+    const eventsResponse = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${eventName}&sort_by=${this.orderby}&categories=${category}&token=${this.auth_token}`);
+
+    // Wait for response and return as json
+
+    const events = await eventsResponse.json();
+
+    return {
+         events
+    }
+}
+
 // Get the Events from API
-async getCategoriesAPI() { 
- 
-    const eventsResponse = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${ this.auth_token }`);
+async getCategoriesAPI() {                                                                                                                                                                                                                                                                                                                                                
+    // Query the API
+     const categoriesResponse = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.auth_token}`);
 
     //hold the response and then return as json
     const categories = await eventsResponse.json();
